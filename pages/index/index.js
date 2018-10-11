71aad7b72299ffc98d50e09b8b8db1605a881cfa
wang sheng <wsheng.980210@gmail.com>
2018-10-11 21:49:17 +0800

    3-5

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,13 @@
-page({})
+Page({
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        console.log(res.data)
+      }
+    })
+  }
+})
